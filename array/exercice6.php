62d9ef1d4b43818c48c6db6494a3adf9db7478c4
dubech <franck.dubech@hotmail.com>
2017-06-21 11:14:36 +0200

    add exo6

---
+++ array/exercice6.php
@@ -0,0 +1,14 @@
+<?php 
+
+	$Departments = array(
+ 
+      02 => 'Aisne', 
+      59 => 'Nord', 
+      60 => 'Oise', 
+      62 => 'Pas-de-Calais',
+      80 => 'Somme '
+  );
+
+	echo $Departments[59];
+
+?>
