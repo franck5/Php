af369cc7f173b66284d2c2eade5bdd80da929c0a
dubech <franck.dubech@hotmail.com>
2017-06-21 11:19:05 +0200

    add exo7

---
+++ array/exercice7.php
@@ -0,0 +1,13 @@
+<?php 
+
+	$Departments = array(
+	 
+	      02 => 'Aisne', 
+	      59 => 'Nord', 
+	      60 => 'Oise', 
+	      62 => 'Pas-de-Calais',
+	      80 => 'Somme '
+	);
+$Departments[51] = "Marne";
+echo $Departments[51];
+?>
