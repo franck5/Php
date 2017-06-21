690f2239cc67d2f9c54472f66f257167d82dbafb
dubech <franck.dubech@hotmail.com>
2017-06-21 11:39:07 +0200

    add exo8

---
+++ array/exercice8.php
@@ -0,0 +1,11 @@
+<?php 
+
+	$mois = array('Janvier','Février','Mars','Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+
+	for($i = 0; $i < count($mois); ++$i) {
+		echo $mois[$i];
+	}
+
+
+
+?>
