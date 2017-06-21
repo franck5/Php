216e28e2f7f4a97c07b12cb5fccaea8d765cddb5
dubech <franck.dubech@hotmail.com>
2017-06-21 11:44:09 +0200

    add exo9

---
+++ array/exercice9.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+	$Departments = array(
+ 
+      02 => 'Aisne', 
+      59 => 'Nord', 
+      60 => 'Oise', 
+      62 => 'Pas-de-Calais',
+      80 => 'Somme '
+  );
+	foreach($Departments as $cle=>$valeur) 
+    { 
+    echo $cle.' : '.$valeur.'<br>'; 
+    } 
+?>
