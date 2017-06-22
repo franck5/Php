70321e5fc90b30b4470aa9554a50a72b4bfa3028
dubech <franck.dubech@hotmail.com>
2017-06-22 12:29:34 +0200

    exo 4

--- globalVariable/recup.php
+++ globalVariable/recup.php
@@ -1,7 +1,24 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h2>Exo2</h2>
+	<?php 
+	session_start();
+		echo $_SESSION['Nom'];
+		echo $_SESSION['Prenom'];
+		echo $_SESSION['Age'];
 
-session_start();
-	echo $_SESSION['Nom'];
-	echo $_SESSION['Prenom'];
-	echo $_SESSION['Age'];
-?>
+	?>
+	<h2>Exo4</h2>
+	<?php
+	echo $_COOKIE['login'];
+	
+	echo $_COOKIE['pass'];
+	?>
+</body>
+</html>
+
+
