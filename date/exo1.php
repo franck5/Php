8a25e9e695d9ddcccf63c6a53ff1eab224c0d611
dubech <franck.dubech@hotmail.com>
2017-06-22 15:22:12 +0200

    exo7

--- date/exo1.php
+++ date/exo1.php
@@ -7,7 +7,7 @@
 	<h2>Exo 1</h2>
 	<?php 
 
-		echo $today = date("d/m/Y");;
+		echo $today = date("d/m/Y");
 
 	?>
 	<h2>Exo 2</h2>
@@ -35,5 +35,29 @@
 	 
 		echo mktime(15, 0, 0, 8, 2, 2016);
 	?>
+
+	<h2>Exo 6</h2>
+
+	<?php 
+
+		$date1 = "22/06/2017";
+		$date2 = "16/05/2016";
+  
+
+		list($jour1, $mois1, $annee1) = explode('/', $date1);
+		list($jour2, $mois2, $annee2) = explode('/', $date2);
+		 
+
+		$timestamp1 = mktime(0,0,0,$mois1,$jour1,$annee1);
+		$timestamp2 = mktime(0,0,0,$mois2,$jour2,$annee2);
+		$nbJours = abs($timestamp2 - $timestamp1)/86400;
+		echo "Nombre de jours : ".$nbJours;
+
+	?>
+	<h2>Exo 7</h2>
+	<?php
+		$number = cal_days_in_month(CAL_GREGORIAN, 2, 2016); // 31
+		echo $number;
+?>
 </body>
 </html>
