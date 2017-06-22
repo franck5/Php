8566d79ac89c64d9d5c09fff9c009aa93c165849
dubech <franck.dubech@hotmail.com>
2017-06-22 12:22:24 +0200

    add exo 3

--- globalVariable/index.php
+++ globalVariable/index.php
@@ -4,7 +4,6 @@
 	<title></title>
 </head>
 <body>
-	<a href="recup.php">Go</a>
 	<h2>Exo1</h2>
 	<?php
  
@@ -24,13 +23,34 @@
 
 	<h2>Exo 2</h2>
 
+	<a href="recup.php">Go</a>
 	<?php 
 		session_start();
 		$_SESSION['Nom'] = 'Dubech';
 		$_SESSION['Prenom'] = 'Franck';
 		$_SESSION['Age'] = 23;
 
+	?>
+	<h2>Exo 3</h2>
+	<form action="#" method="POST">
+		<label for="login">Login</label>
+		<input type="text" name="login">
+		<label for="pass">Mot de passe</label>
+		<input type="password" name="pass">
+		<input type="submit" value="Envoyer">
 
+	</form>
+
+	<?php 
+		if(isset($_POST['login'])){
+
+		    setcookie('login', $_POST['login'], time() + 365*24*3600, null, null, false, true);!
+		    header("Location: index.php");
+		}if(isset($_POST['pass'])){
+
+		    setcookie('pass', $_POST['pass'], time() + 365*24*3600, null, null, false, true);!
+		    header("Location: index.php");
+		}
 
 	?>
 </body>
