955842ee9719314d8a157b19ea0e5e4e057fc38f
dubech <franck.dubech@hotmail.com>
2017-06-22 09:56:41 +0200

    add exo1 form

---
+++ form/user.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+<form action="user.php" method="GET">
+	<label>Nom</label>
+	<input type="text" name="Nom">
+	<label>Prenom</label>
+	<input type="text" name="Prenom">
+	<input type="submit" value="Ok">
+</form>
+
+<!--<?php 
+	$Prenom = $_GET['Prenom']; 
+	$Nom = $_GET['Nom']; 
+	echo("<center>Bonjour $Prenom $Nom</center>"); 
+?> -->
+
+
+</body>
+</html>
