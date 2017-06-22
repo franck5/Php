818dcf5bc7ca1af9f9b738560a63717f52a3043b
dubech <franck.dubech@hotmail.com>
2017-06-22 14:26:56 +0200

    Modif exo 1 et ajout exo 2

--- date/exo1.php
+++ date/exo1.php
@@ -1,5 +1,20 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h2>Exo 1</h2>
+	<?php 
 
-	echo $today = date("d/m/y");;
+		echo $today = date("d/m/Y");;
 
-?>
+	?>
+	<h2>Exo 2</h2>
+	<?php 
+
+		echo $today = date("d-m-y");;
+
+	?>
+</body>
+</html>
