7e65aa72678c0dc22e1c73dec5bbe006c5677f2f
dubech <franck.dubech@hotmail.com>
2017-06-22 11:45:47 +0200

    exo 2

--- globalVariable/index.php
+++ globalVariable/index.php
@@ -4,10 +4,17 @@
 	<title></title>
 </head>
 <body>
+	<h2>Exo1</h2>
 	<?php
  
-echo $_SERVER['HTTP_USER_AGENT'] ;
+		echo $_SERVER['HTTP_USER_AGENT'] ;
  
-?>
+	?>
+	<h2>Exo 2</h2>
+
+	<?php 
+		echo $_SERVER['REMOTE_ADDR']; 
+	?> 
+
 </body>
 </html>
